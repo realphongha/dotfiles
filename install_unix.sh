784115fef3f06af735b0cd6202c25bdca476b9d1
Phong Ha <realphongha@gmail.com>
2024-11-14 10:05:20 +0700

    add wezterm and aerospace installation script

--- install_unix.sh
+++ install_unix.sh
@@ -3,7 +3,10 @@ NVIM_REPO="https://github.com/realphongha/nvim-configs.git"
 NVIM_CFG_PATH="~/.config/nvim"
 NVIM_DATA_PATH="~/.local/share/nvim"
 TMUX_CFG_PATH="~/.tmux.conf"
+WEZTERM_CFG_PATH="~/.wezterm.lua"
+AEROSPACE_CFG_PATH="~/.aerospace.toml"
 
+# neovim
 while true; do
     read -p "Do you wish to install Neovim configs (older configs will be overwritten)? " yn
     case $yn in
@@ -13,8 +16,19 @@ while true; do
     esac
 done
 
+# tmux
 if [ ! -d ~/.tmux/plugins/tpm ] ; then
     git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 fi
 rm $TMUX_CFG_PATH
 cp .tmux.conf $TMUX_CFG_PATH
+
+# wezterm
+rm $WEZTERM_CFG_PATH
+cp .wezterm.lua $WEZTERM_CFG_PATH
+
+# aerospace
+if [ $OS = "Darwin" ]; then
+    rm $AEROSPACE_CFG_PATH
+    cp .aerospace.toml $AEROSPACE_CFG_PATH
+fi
